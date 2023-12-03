0fd1f955d7276b58f89acfe816eca37f5b2c7043
Hayden McCabe <hayden@haydenmccabe.com>
2023-12-03 05:12:35 -0800

    Added solution to Day 3 part 2

--- Day 3/part_2.py
+++ Day 3/part_2.py
@@ -8,9 +8,26 @@ data_lines = data_file.read().splitlines()
 last_line_number = len(data_lines) - 1
 
 number_regex = re.compile("(\d+)")
-symbol_regex = re.compile("[^\s\d\.]+")
+symbol_regex = re.compile("\*")
 
-sum = 0
+# Make a dictionary to track occurances of the *
+# character. The key is a tuple of the line number
+# and the character's string index, and the value
+# is an array of the values adjacent to it.
+gears = {}
+# Use a function to update the gears dictionary
+# This function takes a line number, a range to search
+# in, and the value of the number that called this function.
+def track_gears(line_number: int, range: (int, int), number: int):
+    substring = data_lines[line_number][range[0]:range[1]]
+
+    matches = symbol_regex.finditer(substring)
+    for match in matches:
+        key = (line_number, match.span()[0] + range[0])
+        # Get any existing values for this key, or [] as a default
+        values = gears.get(key, [])
+        values.append(number)
+        gears[key] = values
 
 for line_number in range(len(data_lines)):
     line = data_lines[line_number]
@@ -19,7 +36,7 @@ for line_number in range(len(data_lines)):
     for number in numbers:
         # Track if a symbol has been found near this value
         symbol_found = False
-
+        int_value = int(number.group(1))
         # Find the range to search for a symbol
         # that is extended by one character in
         # each direction.
@@ -27,26 +44,18 @@ for line_number in range(len(data_lines)):
         # AFTER the match and may not be a valid index.
         search_start = max(0, number.span()[0] - 1)
         search_end = min(len(line), number.span()[1]+1)
-        # Search the lines above and below for a symbol
+        # Search the lines above and below
         if line_number > 0:
-            search_substring = data_lines[line_number-1][search_start:search_end]
-            symbol_match = symbol_regex.search(search_substring)
-            if symbol_match is not None:
-                symbol_found = True
+            track_gears(line_number - 1, (search_start, search_end), int_value)
         if line_number < last_line_number:
-            search_substring = data_lines[line_number+1][search_start:search_end]
-            symbol_match = symbol_regex.search(search_substring)
-            if symbol_match is not None:
-                symbol_found = True
-        # Search the characters before and after the number
-        prefix_match = symbol_regex.search(line[search_start])
-        if prefix_match is not None:
-            symbol_found = True
-        suffix_match = symbol_regex.search(line[search_end - 1])
-        if suffix_match is not None:
-            symbol_found = True
-        # If this number corresponds to a symbol, add it to the total
-        if symbol_found:
-            sum += int(number.group(1))
-        
+            track_gears(line_number + 1, (search_start, search_end), int_value)
+        # Search the line where the number was found
+        track_gears(line_number, (search_start, search_end), int_value)
+
+
+sum = 0      
+for position, values in gears.items():
+    if len(values) == 2:
+        sum += values[0] * values[1]
+
 print("Sum: %d" % sum)
